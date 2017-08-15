819cf738ede26233d854935044d3796879c99fce
Timothy Rodgers <rodgers.timothy@gmail.com>
2017-08-15 12:43:04 +0100

    Initial Commit

---
+++ Rational/Rational.h
@@ -0,0 +1,19 @@
+//
+//  Rational.h
+//  Rational
+//
+//  Created by Timothy Rodgers on 15/08/2017.
+//  Copyright © 2017 Timothy Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rational.
+FOUNDATION_EXPORT double RationalVersionNumber;
+
+//! Project version string for Rational.
+FOUNDATION_EXPORT const unsigned char RationalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rational/PublicHeader.h>
+
+
